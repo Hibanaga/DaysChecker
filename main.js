7138cceecaeb0b5a22be38dcb2a136ad4245c8a8
Hibana <pagliaccizeus@gmail.com>
2021-04-20 12:33:22 +0200

    finished program DaysChecker

--- main.js
+++ main.js
@@ -19,6 +19,13 @@ let months = [
   "December",
 ];
 
+document
+  .querySelector(".js-btn-restart")
+  .addEventListener("click", function (e) {
+    document.querySelector(".navForm").innerHTML = navYearInput;
+    location.reload();
+  });
+
 let navYearInput = `
 
 <span class="navSpanQuestion">
@@ -31,10 +38,10 @@ First, in what <span class="spanStrong">year</span> were you born?
   placeholder="enter the year..."
   autocomplete="off"
 />
-<button class="js-submit-year" disabled>Submit</button>
+<button class="js-submit-year">Submit</button>
 </div>`;
 
-for (let i = 0; i <= 100; i++) {
+for (let i = 1; i <= 100; i++) {
   document.querySelector(
     ".currentsYears"
   ).innerHTML += `<button class="js-btn-year" value="${i}">${i}</button>`;
@@ -75,9 +82,6 @@ btn_selectYear.addEventListener("click", function (e) {
     document.querySelector(".navForm").innerHTML = html;
 
     monthRealizeLogic();
-
-    if (monthInfo !== "") {
-    }
   } else {
     // let html = `<span class="spanWrongText"><span class="spanWrongInput">Wrong Input </span> <img src="./images/sad.png" /> </span>`;
     document.querySelector(
@@ -89,18 +93,157 @@ btn_selectYear.addEventListener("click", function (e) {
   }
 });
 
-document
-  .querySelector(".js-btn-restart")
-  .addEventListener("click", function (e) {
-    document.querySelector(".navForm").innerHTML = navYearInput;
-  });
-
 function monthRealizeLogic() {
   document
     .querySelector(".monthBlocks")
     .addEventListener("click", function (e) {
       if (e.target.classList.contains("js-montn-btn")) {
+        // console.log(e.target.value);
         monthInfo = e.target.value;
+        createDayChecker();
       }
     });
+}
+
+function createDayChecker() {
+  let monthNumber = months.indexOf(monthInfo) + 1;
+  let dateValue = "";
+  if (monthNumber < 10) {
+    dateValue = `${yearInfo}-${"0" + monthNumber}-01`;
+  } else {
+    dateValue = `${yearInfo}-${monthNumber}-01`;
+  }
+  let html = `<span class="dayChecker"> On what <span class="spanStrong"> day </span> in ${monthInfo} of ${yearInfo}`;
+  html += ` were you born?`;
+  html += `</span>`;
+  html += `<div class="blockInfoDayDate"> `;
+  html += `<input type="date" name="calendar" class="js-date-input" value="${dateValue}"> `;
+  html += `<button class="js-btn-next"> Next </button>`;
+  html += `</div>`;
+
+  document.querySelector(".navForm").innerHTML = html;
+
+  document
+    .querySelector(".js-btn-next")
+    .addEventListener("click", function (e) {
+      let dateValue = document.querySelector(".js-date-input").value;
+      let infoDate = dateValue.split("-");
+      let [year, month, day] = infoDate;
+
+      let date1 = new Date(year, month, day);
+      let date2 = new Date(
+        date.getFullYear(),
+        date.getMonth() + 1,
+        date.getDate()
+      );
+
+      createFullDateInfo(diffDates(date2, date1));
+    });
+}
+
+function createFullDateInfo(finalDate) {
+  let finalYear = Math.floor(Math.floor(finalDate) / 365);
+  let html = `<span class="spanFullDate spanStrong"> `;
+  html += `You are `;
+  html += `${Math.floor(finalDate)}`;
+  html += ` days old! and it's year is ${finalYear} year!`;
+  html += "</span>";
+  html += lifeComparison(Math.floor(finalDate));
+
+  document.querySelector(".navForm").innerHTML = html;
+}
+
+document.querySelector(".currentsDays").addEventListener("click", function (e) {
+  let daysConvertToYear = 0;
+  if (e.target.classList.contains("js-btn-day")) {
+    daysConvertToYear = e.target.value;
+  }
+
+  let fullInfoDate = calculateTime(daysConvertToYear);
+  let { years, months, weeks, days } = fullInfoDate;
+
+  let html = `<span class="spanCurrentDateConvertDays"> In <span class="spanStrong"> ${daysConvertToYear} </span>`;
+  html += `days old, you'll be`;
+  html += `<span class="spanStrong"> ${years} years, ${months} months,and ${
+    weeks * 7 + days
+  } days </span>`;
+  html += `</span>`;
+
+  document.querySelector(".navForm").innerHTML = html;
+});
+
+document
+  .querySelector(".currentsYears")
+  .addEventListener("click", function (e) {
+    let yearConvertToDays = 0;
+    if (e.target.classList.contains("js-btn-year")) {
+      yearConvertToDays = e.target.value;
+    }
+    let days = calculateTimeYear(yearConvertToDays);
+    let html = `<span class="spanCurrentDateConvertDays"> At <span class="spanStrong"> ${yearConvertToDays} years old, </span>`;
+    html += `you'll be <span class="spanStrong"> ${days} </span> days old!`;
+    html += `</span>`;
+
+    document.querySelector(".navForm").innerHTML = html;
+  });
+
+function lifeComparison(daysLife) {
+  let yearInfo = daysLife / 365;
+
+  let html = `<span class="spanComprassion"> If you are interested here is a comparison in life expectancy with animals </span>`;
+  html += `<span class="spanAnimalsYears"><img src="./images/cat.png" class="imgAnimal" /> <span class="yearsAnimal"> ${(
+    yearInfo * 0.313
+  ).toFixed(2)} year </span>  </span>`;
+  html += `<span class="spanAnimalsYears"><img src="./images/hippopotamus.png" class="imgAnimal" /> <span class="yearsAnimal">${(
+    yearInfo * 0.562
+  ).toFixed(2)} year  </span>  </span>`;
+  html += `<span class="spanAnimalsYears"><img src="./images/rat.png" class="imgAnimal" /> <span class="yearsAnimal">${(
+    yearInfo * 0.05
+  ).toFixed(2)} year  </span>  </span>`;
+  html += `<span class="spanAnimalsYears"><img src="./images/shiba.png" class="imgAnimal" /> <span class="yearsAnimal">${(
+    yearInfo * 0.275
+  ).toFixed(2)} year </span>  </span>`;
+  html += `<span class="spanAnimalsYears"><img src="./images/monkey.png" class="imgAnimal" /> <span class="yearsAnimal">${(
+    yearInfo * 0.313
+  ).toFixed(2)} year </span>  </span>`;
+
+  return html;
+}
+
+function calculateTime(dateInDays) {
+  let months = 0;
+  let years = 0;
+  let days = 0;
+  let weeks = 0;
+
+  while (dateInDays) {
+    if (dateInDays >= 365) {
+      years++;
+      dateInDays -= 365;
+    } else if (dateInDays >= 30) {
+      months++;
+      dateInDays -= 30;
+    } else if (dateInDays >= 7) {
+      weeks++;
+      dateInDays -= 7;
+    } else {
+      days++;
+      dateInDays--;
+    }
+  }
+
+  return {
+    years,
+    months,
+    weeks,
+    days,
+  };
+}
+
+function calculateTimeYear(year) {
+  return year * 365;
+}
+
+function diffDates(dateOne, dateTwo) {
+  return (dateOne - dateTwo) / (60 * 60 * 24 * 1000);
 }
